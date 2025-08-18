8986d29d4dd27283756fd743cd9505fcccf20e81
guiuferrerm <208443537+guiuferrerm@users.noreply.github.com>
2025-08-18 18:01:59 +0200

    initial commit

---
+++ service_worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "gps-openalpinemap-recorder-v1";
+const FILES_TO_CACHE = [
+  "/index.html",
+  "/manifest.json",
+  "/icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
